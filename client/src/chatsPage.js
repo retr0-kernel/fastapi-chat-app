d31c77e5d9df87ba3980373af3846c8fad2af4c5
Krish Srivastava <krish22092003@gmail.com>
2023-04-05 11:30:24 +0530

    fastapi-react

---
+++ client/src/chatsPage.js
@@ -0,0 +1,16 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <PrettyChatWindow
+        projectId="11b05d69-1f2a-4f98-8e14-03e50f099ec2"
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
